02c62f6f3d553c2e0697786f9c318a10054f20b2
Davide Di Mauro <davidedimauro88@gmail.com>
2017-03-30 10:28:32 +0100

    explicit filename

--- test/integration/terraform/serverspec/localhost/tf_spec.rb
+++ test/integration/terraform/serverspec/localhost/tf_spec.rb
@@ -6,6 +6,6 @@ describe file('/usr/local/bin/terraform') do
     it { should be_executable.by('others') }
 end
 
-describe file('/tmp/#{terraform_compressed_file}') do
+describe file('/tmp/terraform_0.9.0_linux_amd64.zip') do
     it { should_not exist }
 end
